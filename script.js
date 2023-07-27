534f5e990542d13980d0281c1cad9e945e69b0a4
Noushadozi <noushadozi333@gmail.com>
2023-07-28 02:12:18 +0600

    css manipulation done

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.round(Math.random() * 20);
 let remainingChance = 5;
+let highScore = 100;
 
 document.querySelector('.check').addEventListener('click', function () {
   let guess = document.querySelector('.guess').value;
@@ -9,12 +10,18 @@ document.querySelector('.check').addEventListener('click', function () {
   if (remainingChance > 0) {
     if (guess == secretNumber) {
       document.querySelector('.message').innerHTML = 'you won!';
+      document.querySelector('.number').innerHTML = secretNumber;
+      document.querySelector('.highscore').innerHTML = highScore;
+      document.querySelector('body').style.backgroundColor = '#60b347';
+
     } else if (guess > secretNumber) {
-      document.querySelector('.message').innerHTML = 'too high';
+      document.querySelector('.message').innerHTML = 'too high  📈';
       remainingChance--;
+      highScore -= 20;
     } else if (guess < secretNumber) {
-      document.querySelector('.message').innerHTML = 'too low';
+      document.querySelector('.message').innerHTML = 'too low 📉';
       remainingChance--;
+      highScore -= 20;
     }
   } else {
     document.querySelector('.message').innerHTML = 'you lost';
