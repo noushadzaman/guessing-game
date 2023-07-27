f1eaef225ac746c102bc38d1707985ee53974f61
Noushadozi <noushadozi333@gmail.com>
2023-07-28 02:26:07 +0600

    bug fixed

--- script.js
+++ script.js
@@ -7,26 +7,29 @@ let highScore = 100;
 document.querySelector('.check').addEventListener('click', function () {
   let guess = document.querySelector('.guess').value;
 
-  if (remainingChance > 0) {
-    if (guess == secretNumber) {
-      document.querySelector('.message').innerHTML = 'you won!';
-      document.querySelector('.number').innerHTML = secretNumber;
-      document.querySelector('.highscore').innerHTML = highScore;
-      document.querySelector('body').style.backgroundColor = '#60b347';
-
-    } else if (guess > secretNumber) {
-      document.querySelector('.message').innerHTML = 'too high  📈';
-      remainingChance--;
-      highScore -= 20;
-    } else if (guess < secretNumber) {
-      document.querySelector('.message').innerHTML = 'too low 📉';
-      remainingChance--;
-      highScore -= 20;
-    }
+  if (!guess) {
+    document.querySelector('.message').innerHTML = '⚠️ no number';
   } else {
-    document.querySelector('.message').innerHTML = 'you lost';
+    if (remainingChance > 0) {
+      if (guess == secretNumber) {
+        document.querySelector('.message').innerHTML = 'you won!';
+        document.querySelector('.number').innerHTML = secretNumber;
+        document.querySelector('.highscore').innerHTML = highScore;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+      } else if (guess > secretNumber) {
+        document.querySelector('.message').innerHTML = '📈 too high';
+        remainingChance--;
+        highScore -= 20;
+      } else if (guess < secretNumber) {
+        document.querySelector('.message').innerHTML = '📉 too low';
+        remainingChance--;
+        highScore -= 20;
+      }
+    } else {
+      document.querySelector('.message').innerHTML = 'you lost';
+    }
+    document.querySelector('.score').innerHTML = remainingChance;
   }
-  document.querySelector('.score').innerHTML = remainingChance;
 });
 
 console.log(remainingChance);
